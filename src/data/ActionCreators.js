f214c7604def25efdcd1b93e934015446268b044
huyHA9597 <haanhuy5@gmail.com>
2022-08-02 20:39:25 +0700

    Import pagination feature without control

--- src/data/ActionCreators.js
+++ src/data/ActionCreators.js
@@ -1,12 +1,16 @@
 import { ActionTypes } from "./Types"
 // import { data as phData } from './placeholderData';
 import { RestDataSource } from "./RestDataSource"
+// import { response } from "express"
 
 const dataSource = new RestDataSource()
 
-export const loadData = (dataType) => ({
+export const loadData = (dataType, params) => ({
 	type: ActionTypes.DATA_LOAD,
-	payload: dataSource
-		.GetData(dataType)
-		.then((response) => ({ dataType, data: response.data })),
+	payload: dataSource.GetData(dataType, params).then((response) => ({
+		dataType,
+		data: response.data,
+		total: Number(response.headers["x-total-count"]),
+		params,
+	})),
 })
--- src/data/Urls.js
+++ src/data/Urls.js
@@ -5,6 +5,6 @@ const hostname = "localhost"
 const port = 3500
 
 export const RestUrls = {
-	[DataTypes.PRODUCTS]: `${protocol}:://${hostname}:${port}/api/products`,
-	[DataTypes.CATEGORIES]: `${protocol}:://${hostname}:${port}/api/categories`,
+	[DataTypes.PRODUCTS]: `${protocol}://${hostname}:${port}/api/products`,
+	[DataTypes.CATEGORIES]: `${protocol}://${hostname}:${port}/api/categories`,
 }
