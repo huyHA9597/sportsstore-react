4e749bfd2a11fe70221336aa1f00163d505345cb
huyHA9597 <haanhuy5@gmail.com>
2022-08-09 06:35:14 +0700

    Import Graphql server

--- server.js
+++ server.js
@@ -1,33 +1,51 @@
-const express = require('express');
-const jsonServer = require('json-server');
-const chokidar = require('chokidar');
-const cors = require('cors');
-const fileName = process.argv[2] || './data.js';
-const port = process.argv[3] || 3500;
+const express = require("express")
+const jsonServer = require("json-server")
+const chokidar = require("chokidar")
+const cors = require("cors")
+const fs = require("fs")
+const { buildSchema } = require("graphql")
+const graphqlHTTP = require("express-graphql")
+const queryResolvers = require("./serverQueriesResolver")
+const mutationResolvers = require("./serverMutationsResolver")
 
-let router = undefined;
+const fileName = process.argv[2] || "./data.js"
+const port = process.argv[3] || 3500
 
-const app = express();
+let router = undefined
+let graph = undefined
+
+const app = express()
 
 const createServer = () => {
-    delete require.cache[require.resolve(fileName)];
-    setTimeout(() => {
-        router = jsonServer.router(
-            fileName.endsWith('.js') ? require(fileName)() : fileName
-        );
-    }, 100);
-};
+	delete require.cache[require.resolve(fileName)]
+	setTimeout(() => {
+		router = jsonServer.router(
+			fileName.endsWith(".js") ? require(fileName)() : fileName
+		)
+		let schema =
+			fs.readFileSync("./serverMutationsSchema.graphql", "utf-8") +
+			fs.readFileSync("./serverQueriesSchema.graphql", "utf-8")
+		let resolvers = { ...queryResolvers, ...mutationResolvers }
+		graph = graphqlHTTP({
+			schema: buildSchema(schema),
+			rootValue: resolvers,
+			graphiql: true,
+			context: { db: router.db },
+		})
+	}, 100)
+}
 
-createServer();
+createServer()
 
-app.use(cors());
-app.use(jsonServer.bodyParser);
-app.use('/api', (req, resp, next) => router(req, resp, next));
+app.use(cors())
+app.use(jsonServer.bodyParser)
+app.use("/api", (req, resp, next) => router(req, resp, next))
+app.use("/graphql", (req, resp, next) => graph(req, resp, next))
 
-chokidar.watch(fileName).on('change', () => {
-    console.log('Reloading web service data...');
-    createServer();
-    console.log('Reloading web service data complete.');
-});
+chokidar.watch(fileName).on("change", () => {
+	console.log("Reloading web service data...")
+	createServer()
+	console.log("Reloading web service data complete.")
+})
 
-app.listen(port, () => console.log(`Web service running on port ${port}`));
+app.listen(port, () => console.log(`Web service running on port ${port}`))
