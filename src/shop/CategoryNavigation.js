c575a45c4aea44dd77ee81423680748e961a4603
huyHA9597 <haanhuy5@gmail.com>
2022-08-13 14:57:17 +0700

    Finalize admin part and deployment

--- src/shop/CategoryNavigation.js
+++ src/shop/CategoryNavigation.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react"
-// import { Link } from 'react-router-dom';
-import { ToggleLink } from "../ToggleLink"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { ToggleLink } from "../ToggleLink";
 
 export class CategoryNavigation extends Component {
 	render() {
@@ -17,7 +17,12 @@ export class CategoryNavigation extends Component {
 							{cat}
 						</ToggleLink>
 					))}
+				<Link
+					className="btn btn-block btn-secondary fixed-bottom m-2 col-3"
+					to="/admin">
+					Administration
+				</Link>
 			</React.Fragment>
-		)
+		);
 	}
 }
